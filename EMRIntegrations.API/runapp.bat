8750c6e3af01f6e793c8775fe763d5c5f219bd21
Jaydip Pal <jaydip@intellihinc.com>
2021-02-12 12:43:31 +0530

    Docker.Config file added

--- EMRIntegrations.API/runapp.bat
+++ EMRIntegrations.API/runapp.bat
@@ -1,4 +1,4 @@
-
+Copy C:\inetpub\wwwroot\webapp\Web.Docker.config C:\inetpub\wwwroot\webapp\Web.config 
 @PowerShell "(GC C:\inetpub\wwwroot\webapp\Web.config)|%%{$_ -Replace '#DB_HOST#',$env:DB_HOST}|SC C:\inetpub\wwwroot\webapp\Web.config"
 @PowerShell "(GC C:\inetpub\wwwroot\webapp\Web.config)|%%{$_ -Replace '#DB_USER#',$env:DB_USER}|SC C:\inetpub\wwwroot\webapp\Web.config"
 @PowerShell "(GC C:\inetpub\wwwroot\webapp\Web.config)|%%{$_ -Replace '#DB_PASSWORD#',$env:DB_PASSWORD}|SC C:\inetpub\wwwroot\webapp\Web.config"
