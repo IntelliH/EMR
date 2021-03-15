ac70257c523d00e8d8fcb93e55d4a1bc016af2f0
Jaydip Pal <jaydip@intellihinc.com>
2021-03-15 14:14:24 +0530

    Added force command

--- EMRIntegrations.API/runapp.bat
+++ EMRIntegrations.API/runapp.bat
@@ -3,6 +3,6 @@ Copy C:\inetpub\wwwroot\webapp\Web.Docker.config C:\inetpub\wwwroot\webapp\Web.c
 @PowerShell "(GC C:\inetpub\wwwroot\webapp\Web.config)|%%{$_ -Replace '#DB_USER#',$env:DB_USER}|SC C:\inetpub\wwwroot\webapp\Web.config"
 @PowerShell "(GC C:\inetpub\wwwroot\webapp\Web.config)|%%{$_ -Replace '#DB_PASSWORD#',$env:DB_PASSWORD}|SC C:\inetpub\wwwroot\webapp\Web.config"
 @PowerShell "(GC C:\inetpub\wwwroot\webapp\Web.config)|%%{$_ -Replace '#DB_NAME#',$env:DB_NAME}|SC C:\inetpub\wwwroot\webapp\Web.config"
-@PowerShell "Import-Module WebAdministration; Remove-Website -Name 'Default Web Site'; New-Website -Name 'webapp' -Port 80 -PhysicalPath 'C:\inetpub\wwwroot\webapp'; Start-Website -Name webapp; Get-ChildItem -Path IIS:\Sites"
+@PowerShell "Import-Module WebAdministration; Remove-Website -Name 'Default Web Site'; New-Website -Name 'webapp' -Port 80 -PhysicalPath 'C:\inetpub\wwwroot\webapp' -Force; Start-Website -Name webapp; Get-ChildItem -Path IIS:\Sites"
 
 C:\ServiceMonitor.exe w3svc
